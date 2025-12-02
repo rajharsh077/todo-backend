f565f5d1493be2ad8cf4cd318cb380fc572ee499
Harsh <rajharsh9047@gmail.com>
2025-12-02 20:12:06 +0530

    initial

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,17 @@ app.get("/todos",async (req,res)=>{
     res.json(await todo.find());
 })
 
+app.post('/todos', async (req,res) => {
+    const todo = await todo.create({ text : req.body.text });
+    res.json(todo);
+})
+
+app.delete("/todo/:id", async (req, res) => {
+    await todo.findByIdAndDelete(req.params.id);
+    res.json({message : "Deleted"})
+})
+
+
 app.listen(process.env.PORT,()=>{
     console.log(`running on ${process.env.PORT}`);
 })
